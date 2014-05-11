3b514562936786d0f677b563226e3b7acc761f84
Andy Noblet <andrewbnoblet@gmail.com>
2014-05-11 16:53:52 -0400

    Create a simple autoloader.

---
+++ autoloader.php
@@ -0,0 +1,5 @@
+<?php
+function autoload($class) {
+	include($class . ".php");
+}
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+// Let's create an autoloader
+require_once ("autoloader.php");
+spl_autoload_register("autoload");
+?>
