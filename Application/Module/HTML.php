2077f4ad551c27e023de9c40fff553de3bd7bb40
Andy Noblet <andrewbnoblet@gmail.com>
2014-08-10 19:29:56 -0400

    Add changes.

--- Application/Module/HTML.php
+++ Application/Module/HTML.php
@@ -1,117 +1,39 @@
 <?php
+namespace Application\Module {
 
-	Namespace Application\Module
-	{
-		Class HTML Extends \Application\Module
-		{
-			public function toHTML($data = Null, $node = Null, $document = Null)
-			{				
-				if(isset($document));
-				Else
-				{
-					$document = New \DOMDocument("1.0", "UTF-8");
-					$document->preserveWhiteSpace = false;
-					$document->formatOutput = true;
-					
-					$root = $document->createElement($data->__getClass());
-					
-					$attributes = $data->__getAttributes();
-					
-					if(isset($attributes))
-					{
-						ForEach($attributes as $attribute => $attribute_Value)
-						{
-							$attribute = StrToLower($attribute);
-							$root->setAttribute($attribute, $attribute_Value);
-						}
-					}
-					
-					$this->toHTML($data, $root, $document);
-					$document->appendChild($root);
-										
-					$Result = Null;
-					
-					foreach($document->childNodes as $node)
-					{
-						$Result .= $document->saveXML($node)."\n";
-					}
-										
-					$Implementation = new \DOMImplementation();
-					
-						$DTD = $Implementation->createDocumentType('html',
-	                  '-//W3C//DTD XHTML 1.1//EN',
-	                  'http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd');
-						
-						$New_Document = $Implementation->createDocument('http://www.w3.org/1999/xhtml',
-		                   'html',
-		                   $DTD);
-					
-					// Quick hack for errors
-					
-					$New_Document->loadXML($Result);
-					
-					$HTML = $New_Document->saveHTML();
-					
-					Return $HTML;
-				}
+	class HTML extends \Application\Module {
+		public $document;
+		public function toHTML($data = null, $document = null) {
+			if (isset($document));
+			else {
+				$document = new \DOMDocument();
+				$document->formatOutput = true;
 				
-				if(Is_String($data));
-				Else
-				{
-					if(Is_Array($data));
-					Else
-					{
-						if(Is_Object($data))
-						{
-							ForEach($data as $Property => $Value)
-							{
-								$Property = StrToLower($Property);
-								
-								if(Is_String($Value))
-								{
-									$Child = $document->createElement($Property, $Value);
-									$node->appendChild($Child);
-								}
-								Else
-								{
-									if(Is_Array($Value))
-									{										
-										ForEach($Value as $Array_Child)
-										{		
-											$this->toHTML($Array_Child, $node, $document);
-										}
-									}
-									Else
-									{
-										if(Is_Object($Value))
-										{
-											$Child = $document->createElement($Property);
-											
-											$attributes = $Value->__getAttributes();
-
-											if(isset($attributes))
-											{
-												ForEach($attributes as $attribute => $attribute_Value)
-												{
-													$attribute = StrToLower($attribute);
-													$Child->setAttribute($attribute, $attribute_Value);
-												}
-											}
-											
-											$this->toHTML($Value, $Child, $document);
-											$node->appendChild($Child);
-										}
-									}
-								}
-							}
-							
-						}
-					}
-				}
-
-				Return $node;
+				$child = $this->toHTML($data, $document);
+				$document->appendChild($child);
+				
+				$html = $document->saveHTML();
+				
+				return $html;
 			}
+			
+			if (is_string($data)) {
+				// $element = $document->createTextNode
+			} elseif (is_array($data)) {
+				foreach ($data as $property => $value) {
+					$elements[] = $this->toHTML($value, $document);
+				}
+			} elseif (is_object($data)) {
+				$element = $document->createElement($data->__getClass());
+				foreach ($data as $property => $value) {
+					$child = $this->toHTML($value, $document);
+					$element->appendChild($child);
+				}
+			}
+			
+			return $element;
 		}
 	}
+}
 
 ?>
--- application.php
+++ application.php
@@ -5,24 +5,29 @@ class Application {
 		// print $this->model;
 	}
 	public function run() {
-		$html = new Application\Model\HTML;
-		$html->head = new Application\Model\Head;
-		$link = new Application\Model\Link;
+		$html = $this->getModel("Application\\Model\\HTML;");
+		$html->head = $this->getModel("Application\\Model\\Head;");
+		$link = $this->getModel("Application\\Model\\Link;");
 		$link->setAttribute("href", "Application/Library/bootstrap/css/bootstrap.min.css");
 		$html->head->links[] = $link;
-		$link = new Application\Model\Link;
+		$link = $this->getModel("Application\\Model\\Link");
 		$link->setAttribute("href", "style.css");
 		$html->head->links[] = $link;
-		$script = new Application\Model\Script;
+		$script = $this->getModel("Application\\Model\\Script;");
 		$script->setAttribute("src","Application/Library/bootstrap/js/bootstrap.min.js");
 		//$html->head->scripts[] = $script;
-		$html->body = new Application\Model\Body;
+		$html->body = $this->getModel("Application\\Model\\Body;");
 		// $html->body->application = $this->model;
-		$template = new Application\Model\Template;
+		$template = $this->getModel("Application\\Model\\Template;");
 		$template->setAttribute("href", "Application/Template/Layout.html");
 		$html->body->template = $template;
 		
 		print $html;
 	}
+
+    public function getModel($model) {
+        $model = new $model;
+        return $model;
+    }
 }
 ?>
