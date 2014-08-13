5680a0e1b2e7c0ae55a1f10147c2a420355b933f
Andy Noblet <andrewbnoblet@gmail.com>
2014-08-12 22:12:41 -0400

    Add changes.

--- application.php
+++ application.php
@@ -5,20 +5,20 @@ class Application {
 		// print $this->model;
 	}
 	public function run() {
-		$html = $this->getModel("Application\\Model\\HTML;");
-		$html->head = $this->getModel("Application\\Model\\Head;");
-		$link = $this->getModel("Application\\Model\\Link;");
+		$html = $this->getModel("Application\\Model\\HTML");
+		$html->head = $this->getModel("Application\\Model\\Head");
+		$link = $this->getModel("Application\\Model\\Link");
 		$link->setAttribute("href", "Application/Library/bootstrap/css/bootstrap.min.css");
 		$html->head->links[] = $link;
 		$link = $this->getModel("Application\\Model\\Link");
 		$link->setAttribute("href", "style.css");
 		$html->head->links[] = $link;
-		$script = $this->getModel("Application\\Model\\Script;");
+		$script = $this->getModel("Application\\Model\\Script");
 		$script->setAttribute("src","Application/Library/bootstrap/js/bootstrap.min.js");
 		//$html->head->scripts[] = $script;
-		$html->body = $this->getModel("Application\\Model\\Body;");
+		$html->body = $this->getModel("Application\\Model\\Body");
 		// $html->body->application = $this->model;
-		$template = $this->getModel("Application\\Model\\Template;");
+		$template = $this->getModel("Application\\Model\\Template");
 		$template->setAttribute("href", "Application/Template/Layout.html");
 		$html->body->template = $template;
 		
