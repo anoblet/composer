25209a5f6b20fc4da07115fa5641dedffdfe61ef
Andy Noblert <andrewbnoblet@gmail.com>
2016-04-18 12:27:45 -0400

    Update

--- Application.php
+++ Application.php
@@ -1,54 +1,44 @@
 <?php
-	require_once("autoloader.php");
-	
-	Class Application Extends Application\Application_Abstract
-	{	
-		Public Function Initialize()
-		{
-			/*
-			// Let's grab our model
-			$Application = New Application\Model();
-			$Application->Load("Application.xml");
-			*/
-		
-			Application::$Resource = $this;
-		
-			$this->setRuntimeParameters();
-			$this->setErrorReporting();
-			
-			$this->Import("Application.xml");
-			// $Data = $this->Datasource->Load($this);
-			// $this->Import($Data);
+require_once("autoloader.php");
 
-			var_dump($this);
+Class Application Extends Application\Application_Abstract
+{
+    Public Function Initialize() {
+        Application::$Resource = $this;
 
-			$this->Controller = New Application\Controller();
-		
-			$Request = New Application\Module\Request();
-			$Request->Initialize();
-						
-			Print $this->Controller->Execute($Request);
-					
-		}
-		
-		public function setRuntimeParameters()
-		{
-		    Defined('BASE_DIRECTORY') || Define('BASE_DIRECTORY', RealPath(DirName(__FILE__) . ''));
-		    
-		    Set_Include_Path
-		    (
-		    RealPath("./") . PATH_SEPARATOR.
-		    Get_Include_Path()
-		    );
-		    
-		    $this->setErrorReporting();
-		}
-		
-		public function setErrorReporting()
-		{
-		    ini_set('display_errors', 1);
-		    error_reporting(E_ALL);
-		}
-	}
-	
+        $this->setRuntimeParameters();
+        $this->setErrorReporting();
+
+        $this->Import("Application.xml");
+
+        //$Data = $this->Datasource->Load($this);
+        //$this->Import($Data);
+
+        $this->Controller = New Application\Controller();
+
+        $Request = New Application\Module\Request();
+        $Request->Initialize();
+
+        Print $this->Controller->Execute($Request);
+
+    }
+
+    public function setRuntimeParameters() {
+        Defined('BASE_DIRECTORY') || Define('BASE_DIRECTORY', RealPath(DirName(__FILE__) . ''));
+
+        Set_Include_Path
+        (
+            RealPath("./") . PATH_SEPARATOR .
+            Get_Include_Path()
+        );
+
+        $this->setErrorReporting();
+    }
+
+    public function setErrorReporting() {
+        ini_set('display_errors', 1);
+        error_reporting(E_ALL);
+    }
+}
+
 ?>
