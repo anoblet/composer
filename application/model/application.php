a5048122aa7ba0383f19697c3b055422a01e6ead
Andy Noblet <andrewbnoblet@gmail.com>
2017-05-08 09:45:51 -0400

    Update

--- application/model/application.php
+++ application/model/application.php
@@ -2,14 +2,5 @@
 namespace Application\Model;
 
 class Application extends \Application\Model {
-    protected $title;
-
-    /**
-     * @return mixed
-     */
-    public function getTitle()
-    {
-        return $this->title;
-    }
 }
 ?>
--- autoloader.php
+++ autoloader.php
@@ -1,7 +1,7 @@
 <?php
 function Autoload($class)
 {
-    $class = str_replace("\\", DIRECTORY_SEPARATOR, strtolower($class));
+    $class = strtolower($class);
     $file = $class . ".php";
     if (file_exists($file)) {
         include($file);
