c2d2de661a84ec6258caa075c62009ec728904d6
Andy Noblet <andrewbnoblet@gmail.com>
2017-05-08 17:47:42 -0400

    Update

--- Application/Model/Application.php
+++ Application/Model/Application.php
@@ -2,5 +2,12 @@
 namespace Application\Model;
 
 class Application extends \Application\Model {
+    protected $Title;
+    
+    public function getTitle()
+    {
+        return $this->Title;
+    }
+
 }
 ?>
