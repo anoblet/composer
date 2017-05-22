53d4abb1330b2db4dceed781ac6649045c03e300
Andy Noblet <andrewbnoblet@gmail.com>
2017-05-21 20:13:36 -0400

    Update

--- Application/Model/Application.php
+++ Application/Model/Application.php
@@ -2,7 +2,16 @@
 namespace Application\Model;
 
 class Application extends \Application\Model {
-    protected $Title;
+    protected $Title = "My Site";
+    protected $Copyright = "&copy; Andy Noblet 2016";
+
+    /**
+     * @return string
+     */
+    public function getCopyright()
+    {
+        return $this->Copyright;
+    }
 
     public function getTitle()
     {
--- Autoload.php
+++ Autoload.php
@@ -1,7 +1,7 @@
 <?php
 function Autoload($class)
 {
-    $class = str_replace("\\", DIRECTORY_SEPARATOR, strtolower($class));
+    $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
     $file = $class . ".php";
     if (file_exists($file)) {
         include($file);
