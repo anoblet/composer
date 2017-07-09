cc94ebfa82103933590d2313ce3dff638573dbc5
Andy Noblet <andrewbnoblet@gmail.com>
2017-07-09 14:23:30 -0400

    Update

--- Application.js
+++ Application.js
@@ -12,12 +12,24 @@ jQuery(document).ready(function () {
 
     });
     jQuery("#Navigation select").selectmenu({
-        change: function() {
+        change: function () {
             jQuery("#center").load(jQuery(this).val());
         }
     });
     jQuery("select").on("change,", function () {
         jQuery("#center").load(jQuery(this).attr("href"));
     });
+    jQuery(document).on("submit", "form", function (e) {
+        var form = this;
+        e.preventDefault();
+        // jQuery(this).load(jQuery(this).attr("action"), jQuery(this).serialize());
+        jQuery.ajax({
+            url: jQuery(this).attr("action"),
+            method: "POST",
+            data: jQuery(this).serialize()
+        }).done(function (response) {
+            jQuery(form).replaceWith(response);
+        });
+    })
 })
 ;
