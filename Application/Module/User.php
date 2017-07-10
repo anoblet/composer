678cbe858b4eca623cc97a4325c0148f0234f0b4
Andy Noblet <andrewbnoblet@gmail.com>
2017-07-10 09:01:48 -0400

    Login update.

--- Application/Module/User.php
+++ Application/Module/User.php
@@ -2,20 +2,38 @@
 
 namespace Application\Module {
 
-    class User extends \Application\Module
-    {
+    class User extends \Application\Module {
         public function Index() {
             return $this->Login();
         }
+
         public function Login() {
-            var_dump($this->getRequest());
-            if (isset($User) && isset($Password)) ;
+            $Request = $this->getRequest();
+            $User = $this->getModel("User");
+            $User->setEmail($Request['Arguments']['Email']);
+            $User->setPassword($Request['Arguments']['Password']);
+
+            $Error = null;
+
+            $Email = $User->getEmail();
+            if (isset($Email)) ;
             else {
-                $View = $this->getView("Login.phtml");
+                $Error = "No email given.";
+            };
+            $Password = $User->getPassword();
+            if(isset($Password)) ;
+            else {
+                var_dump($User->getPassword());
+                $Error = "No password given.";
             }
+
+            $User->Error = $Error;
+
+            $View = $this->getView("Login.phtml", array("User" => $User));
 
             return $View;
         }
+
         protected function Info() {
             $User = $this->getModel("User");
             $View = $this->getView("User.phtml", array("User" => $User));
