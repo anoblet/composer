4d298b90dc9b2499d49a4e1fe1fde38df379a434
Andy Noblet <andrewbnoblet@gmail.com>
2017-07-18 18:19:34 -0400

    Move index.php into a separate folder.

--- Autoload.php
+++ Autoload.php
@@ -2,7 +2,7 @@
 function Autoload($class)
 {
     $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
-    $file = $class . ".php";
+    $file = __DIR__ . DIRECTORY_SEPARATOR . $class . ".php";
     if (file_exists($file)) {
         include($file);
     }
